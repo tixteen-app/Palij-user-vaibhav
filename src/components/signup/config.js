ffaf3e2687c9cea6b8ec0fb9548fe18654b984b1
vaibhav_rathore <vaibahvrathorema@gmail.com>
2025-02-25 15:07:22 +0530

    update firebase crdetial

--- src/components/signup/config.js
+++ src/components/signup/config.js
@@ -4,12 +4,21 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAdi-nU_B4WzbIOl1H5pDbibr7VJCMX4WY",
-  authDomain: "login1-aca5b.firebaseapp.com",
-  projectId: "login1-aca5b",
-  storageBucket: "login1-aca5b.firebasestorage.app",
-  messagingSenderId: "317774159034",
-  appId: "1:317774159034:web:ed41f806b08412b8fff683"
+  // vaibhav
+  // apiKey: "AIzaSyAdi-nU_B4WzbIOl1H5pDbibr7VJCMX4WY",
+  // authDomain: "login1-aca5b.firebaseapp.com",
+  // projectId: "login1-aca5b",
+  // storageBucket: "login1-aca5b.firebasestorage.app",
+  // messagingSenderId: "317774159034",
+  // appId: "1:317774159034:web:ed41f806b08412b8fff683"
+  // palji-offical
+  apiKey: "AIzaSyAb3jCXev7EbHC1bRYTGTEfUSgux1rJZm4",
+  authDomain: "palji-web.firebaseapp.com",
+  projectId: "palji-web",
+  storageBucket: "palji-web.firebasestorage.app",
+  messagingSenderId: "666469184305",
+  appId: "1:666469184305:web:a999032ed4c6a5156f3610",
+  measurementId: "G-0H5XVZ45WS"
 };
 
 // Initialize Firebase
