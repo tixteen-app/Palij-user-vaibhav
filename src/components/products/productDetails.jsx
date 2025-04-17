518a80e7c1afc597c02712db6c6aac3db3bd89a4
thevaibhavrr <vaibhavrathorema@gmail.com>
2025-04-17 18:48:36 +0530

    cajhnge poostion of ingriden t

--- src/components/products/productDetails.jsx
+++ src/components/products/productDetails.jsx
@@ -1831,9 +1831,9 @@ function ProductDetails() {
               <p className={styles.description} >{product?.description}</p>
             </div>
             {product?.category?._id === "67b451f7ec3a4e4a3bbe5633" && (
-                <div className={styles.pincodeCheckContainer}>
-                  <div className={styles.pincodeCheckTitle}>Check Delivery Availability</div>
-                  <div className={styles.pincodeInputGroup}>
+              <div className={styles.pincodeCheckContainer}>
+                <div className={styles.pincodeCheckTitle}>Check Delivery Availability</div>
+                <div className={styles.pincodeInputGroup}>
                   {!isHoveringImage ? <>
                     <input
                       type="text"
@@ -1843,7 +1843,7 @@ function ProductDetails() {
                       maxLength={6}
                       className={styles.pincodeInput}
                     />
-                    </> : <>
+                  </> : <>
                     <input
                       type="text"
                       value={pincodeInput}
@@ -1852,39 +1852,39 @@ function ProductDetails() {
                       maxLength={6}
                       className={styles.pincodeInput}
                     />
-                    </>
-                    }
-                    <button
-                      onClick={checkPincodeAvailability}
-                      disabled={checkingPincode}
-                      className={styles.pincodeCheckButton}
-                    >
-                      {checkingPincode ? 'Checking...' : 'Check'}
-                    </button>
-                  </div>
-                  {pincodeCheckResult !== null && (
-                    <div className={`${styles.pincodeResult} ${pincodeCheckResult ? styles.pincodeAvailable : styles.pincodeNotAvailable
-                      }`}>
-                      {pincodeCheckResult ? (
-                        <>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                          </svg>
-                          Available for delivery at this pincode
-                        </>
-                      ) : (
-                        <>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                          </svg>
-                           Not available for delivery at this pincode
-                        </>
-                      )}
-                    </div>
-                  )}
+                  </>
+                  }
+                  <button
+                    onClick={checkPincodeAvailability}
+                    disabled={checkingPincode}
+                    className={styles.pincodeCheckButton}
+                  >
+                    {checkingPincode ? 'Checking...' : 'Check'}
+                  </button>
                 </div>
-              )}
+                {pincodeCheckResult !== null && (
+                  <div className={`${styles.pincodeResult} ${pincodeCheckResult ? styles.pincodeAvailable : styles.pincodeNotAvailable
+                    }`}>
+                    {pincodeCheckResult ? (
+                      <>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                        </svg>
+                        Available for delivery at this pincode
+                      </>
+                    ) : (
+                      <>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                        Not available for delivery at this pincode
+                      </>
+                    )}
+                  </div>
+                )}
+              </div>
+            )}
 
 
             <div className={styles.description}>
@@ -1904,6 +1904,25 @@ function ProductDetails() {
                     <li key={id}>{item?.include}</li>
                   ))}
                 </ul>
+              </div>
+            )}
+
+
+            {product?.ingridents && (
+              <div className={styles.product_details_new_section}>
+                <h2>INGREDIENTS</h2>
+                <p className={styles.product_details_new_text}>
+                  {product.ingridents}
+                </p>
+              </div>
+            )}
+
+            {product?.allergen && (
+              <div className={styles.product_details_new_section}>
+                <h2>ALLERGENS</h2>
+                <p className={styles.product_details_new_text}>
+                  {product.allergen}
+                </p>
               </div>
             )}
 
@@ -1929,23 +1948,7 @@ function ProductDetails() {
               )
             }
 
-{product?.ingridents && (
-  <div className={styles.product_details_new_section}>
-    <h2>INGREDIENTS</h2>
-    <p className={styles.product_details_new_text}>
-      {product.ingridents}
-    </p>
-  </div>
-)}
 
-{product?.allergen && (
-  <div className={styles.product_details_new_section}>
-    <h2>ALLERGENS</h2>
-    <p className={styles.product_details_new_text}>
-      {product.allergen}
-    </p>
-  </div>
-)}
 
           </div>
         </div>
