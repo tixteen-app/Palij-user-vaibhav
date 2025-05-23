c9ec262a4d3ac19ca29ba4a0b3ee354ff85de9d4
thevaibhavrr <vaibhavrathorema@gmail.com>
2025-05-23 11:14:28 +0530

    updat explore icon

--- src/components/SecondHomePage/Explore/Explore.jsx
+++ src/components/SecondHomePage/Explore/Explore.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router';
 import { assets } from '../../../assets/assets'
 import styles from './Explore.module.css'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 const Explore = () => {
   const navigate = useNavigate()
   const handleCategoryClick = () => {
@@ -12,21 +12,23 @@ const Explore = () => {
     <div className={styles.container}>
       <div className={styles.imageContainer}>
         <motion.img
-        initial={{ opacity: 0 , scale: 0.8}}
-        whileInView={{ opacity: 1 , scale: 1}}
-        transition={{ duration: 0.6 }}
-        src={assets.homeGiftHamper} alt="" />
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+          src={assets.homeGiftHamper} alt="" />
       </div>
       <motion.div className={styles.contant}
-        initial={{ opacity: 0 , y: 100}}
-        whileInView={{ opacity: 1 , y: 0}}
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.9 }}
       >
         <h2>More Than a Gift, <br />
           It's a Memory!
         </h2>
         <p>Perfectly Packed for Every Occasion!</p>
-        <button onClick={handleCategoryClick} >EXPLORE <img  src={assets.crossArrow} alt="" /></button>
+        <button onClick={handleCategoryClick} >EXPLORE<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-arrow-up-right explore_icon_home_page" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+        </svg></button>
       </motion.div>
     </div>
   )
